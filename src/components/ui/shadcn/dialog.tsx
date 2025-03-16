a37cd6f23372f6b55f32c221378206a6084c1391
penn <penn201500@gmail.com>
2025-03-16 14:52:28 +0800

    🐛 Fix DialogContent error in dialog.tsx while clicking menu icon on mobile screen
    true

--- src/components/ui/shadcn/dialog.tsx
+++ src/components/ui/shadcn/dialog.tsx
@@ -54,6 +54,7 @@ function DialogContent({
   return (
     <DialogPortal data-slot="dialog-portal">
       <DialogOverlay />
+      <DialogTitle />
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
